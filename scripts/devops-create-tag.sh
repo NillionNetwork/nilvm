1f9ada0a175225bdf6b333953b94f06b8622f372
Jeff Wolski <jeff.wolski@gmail.com>
2025-03-03 12:44:03 +0100

    chore: fix paths to `release_management.sh`

--- scripts/devops-create-tag.sh
+++ scripts/devops-create-tag.sh
@@ -5,7 +5,7 @@ set -o errexit
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}" 2>/dev/null)" && pwd -P)"
 
 # shellcheck disable=SC1091
-source "$SCRIPT_PATH"/../devops/release-management/scripts/include/release_management.sh
+source "$SCRIPT_PATH"/include/release_management.sh
 
 usage() {
     cat <<EOF >&2
