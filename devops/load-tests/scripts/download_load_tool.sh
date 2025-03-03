1667616d4c7302dda02ab41e73b80725882c6ec2
Jeff Wolski <jeff.wolski@gmail.com>
2025-03-03 12:30:24 +0100

    chore: remove devops load-tests

--- devops/load-tests/scripts/download_load_tool.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-PRIVATE_TOOLS=nillion-private-tools-x86_64-unknown-linux-musl.tar.gz
-
-DOWNLOAD_PATH="${1:?DOWNLOAD_PATH is required}"
-RELEASE_VERSION="${2:?RELEASE_VERSION is required}"
-
-aws s3 cp --no-progress "s3://nillion-private-releases/$RELEASE_VERSION/$PRIVATE_TOOLS" "$DOWNLOAD_PATH"
-tar xfvz "$DOWNLOAD_PATH/$PRIVATE_TOOLS" -C "$DOWNLOAD_PATH"
